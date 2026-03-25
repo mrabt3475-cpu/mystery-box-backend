d668c5202af9a9d4c30498eb875d0030ce5e4f4d
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-25 22:21:54 +0100

    feat: create Admin Module

---
+++ src/modules/admin/admin.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AdminController } from './admin.controller';
+
+@Module({
+  controllers: [AdminController],
+  providers: [],
+  exports: [],
+})
+export class AdminModule {}
+
