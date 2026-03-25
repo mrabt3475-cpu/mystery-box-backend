819f88cf0dacdff3935e20610eedd415995ba963
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-25 21:32:41 +0100

    fix: update Boxes Module with controller

--- src/modules/boxes/boxes.module.ts
+++ src/modules/boxes/boxes.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { BoxesController } from './boxes.controller';
 import BoxesService from './boxes.service';
 import AntiAbuseModule from '../anti-abuse/anti-abuse.module';
 import RewardsModule from '../rewards/rewards.module';
 
 @Module({
   imports: [AntiAbuseModule, RewardsModule],
+  controllers: [BoxesController],
   providers: [BoxesService],
   exports: [BoxesService],
 })
 export class BoxesModule {}
+
