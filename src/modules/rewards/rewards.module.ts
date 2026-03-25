f946a5eb60d8472c8f0508f20c5c784d7c7fb3a2
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-25 20:08:07 +0100

    fix: rewards module - add QueueModule

--- src/modules/rewards/rewards.module.ts
+++ src/modules/rewards/rewards.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { RewardsService } from './rewards.service';
+import RewardsService from './rewards.service';
+import QueueModule from '../../queue/queue.module';
 
 @Module({
+  imports: [QueueModule],
   providers: [RewardsService],
   exports: [RewardsService],
 })
