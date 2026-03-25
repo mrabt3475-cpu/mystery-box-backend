e0f0a468cc1e877b44e6e8f1666ff3674949c4ff
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-25 21:32:41 +0100

    fix: update Auth Module with controller

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jut';
 
 @Module({
-  imports: [JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '7d' } })],
+  imports: [JwtModule.register({secret: 'secret', signOptions: { expiresIn: '7d' }})],
+  controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
+
