f1b756368e3db6ed2cf8f745f3a3954d34280f69
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-25 21:32:45 +0100

    fix: update Orders Module with controller

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { AntiAbuseModule } from '../anti-abuse/anti-abuse.module';
 
 @Module({
   imports: [AntiAbuseModule],
+  controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
 })
 export class OrdersModule {}
+
