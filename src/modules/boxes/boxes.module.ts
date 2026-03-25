9c83436133d2801ef99f082e936c227002514693
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-25 20:08:09 +0100

    fix: boxes module - add RewardsModule

--- src/modules/boxes/boxes.module.ts
+++ src/modules/boxes/boxes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { BoxesService } from './boxes.service';
-import { AntiAbuseModule } from '../anti-abuse/anti-abuse.module';
+import BoxesService from './boxes.service';
+import AntiAbuseModule from '../anti-abuse/anti-abuse.module';
+import RewardsModule from '../rewards/rewards.module';
 
 @Module({
-  imports: [AntiAbuseModule],
+  imports: [AntiAbuseModule, RewardsModule],
   providers: [BoxesService],
   exports: [BoxesService],
 })
