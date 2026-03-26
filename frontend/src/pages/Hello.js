62c21bff59bceb48ba0d2b5c5e397af220422864
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-26 20:21:30 +0100

    feat: create Hello page

---
+++ frontend/src/pages/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <div className="hello-page">
+      <h1>Welcome to MysteryBox</h1>
+      <p>Sign in to start winning free products!</p>
+    </div>
+  );
+}
+
+export default Hello;
+..
