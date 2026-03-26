c2c0d611f5a7796b0885438c49d871145fc93a33
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-26 20:21:17 +0100

    feat: create Footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <p>Copyright 2026 MysteryBox. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+.
