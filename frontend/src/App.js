abe8eff84b531233d700f82ad06c53130d5e565e
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-26 21:14:23 +0100

    feat: update App.js with products route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Routes, Redirect } from 'react-router';
-import { AuthContext } from './context/AuthContext';
-import Hello s from './pages/Hello';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import Boxes from './pages/Boxes';
-import OpenBox from './pages/OpenBox';
-import Profile from './pages/Profile';
-import Wallet from './pages/Wallet';
-import Leaderboard from './pages/Leaderboard';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+// Add Products Page to App
+	import Products from './pages/Products';
+	import { Link } from 'react-router';
 
-function App() {
+	function App() {
   return (
-   <BrowserRouter>
-    <AuthContext>
-      <Wallet>
-        <Navbar />
-        <Routes>
-          <Redirect path="/" to={+Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component=[Register] />
-          <Route path="/home" component={Home} />
-          <Route path="/boxes" component={Boxes} />
-          <Route path="/open/:boxId" component={OpenBox} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/vallet" component={Wallet} />
-          <Route path="/leaderboard" component={Leaderboard} />
-        </Routes>
-        <Footer />
+    <BrowserRouter>
+      <AuthContext>
+        <Wallet>
+          <Navbar />
+          <Routes>
+            <redirect path="/" to={+Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component=[Register] />
+            <Route path="/products" component={Products} />
+            <Route path="/boxes" component={Boxes} />
+            <Route path="/open/:boxId" component={OpenBox} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/vallet" component={Wallet} />
+            <Route path="/leaderboard" component={Leaderboard} />
+          </Routes>
+          <Footer />
         </Wallet>
       </AuthContext>
     </BrowserRouter>
