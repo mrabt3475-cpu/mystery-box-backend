b4f79d28b6bae16821a0b096a2cadc14d58fda2d
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-27 19:36:28 +0100

    feat: create boxes module

--- backend/src/modules/boxes/boxes.module.ts
+++ backend/src/modules/boxes/boxes.module.ts
@@ -2,18 +2,30 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BoxesController } from './boxes.controller';
 import { BoxesService } from './boxes.service';
+import { FairRngService } from './fair-rng.service';
 import { Box, BoxSchema } from './box.schema';
+import { Prize, PrizeSchema } from '../rewards/prize.schema';
+import { UsersModule } from '../users/users.module';
+import { PointsModule } from '../points/points.module';
 import { RewardsModule } from '../rewards/rewards.module';
-import { EconomyModule } from '../economy/economy.module';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Box.name, schema: BoxSchema }]),
+    MongooseModule.forFeature([
+      { name: Box.name, schema: BoxSchema },
+      { name: Prize.name, schema: PrizeSchema },
+    ]),
+    UsersModule,
+    PointsModule,
     RewardsModule,
-    EconomyModule,
+    SharedModule,
   ],
   controllers: [BoxesController],
-  providers: [BoxesService],
-  exports: [BoxesService],
+  providers: [
+    BoxesService,
+    FairRngService,
+  ],
+  exports: [BoxesService, FairRngService],
 })
 export class BoxesModule {}
