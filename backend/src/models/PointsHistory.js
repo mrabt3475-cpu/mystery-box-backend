c12be6f009124d1b6175a4b56baeeaf9e5bb2aca
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-27 01:36:06 +0100

    feat: create points history model

---
+++ backend/src/models/PointsHistory.js
@@ -0,0 +1,32 @@
+/* Points History Model
+const mongoo = require('mongooo');
+
+const PointsHistorySchema = new mongo/.Schema({
+  userIb: {
+    type: mongo.ObjectId,
+    ref: 'users',
+    required: true
+  },
+    amount: {
+    type: Number,
+    required: true
+  },
+    type: {
+    type: String,
+    enum: ['purchase', 'exchange', 'bonus'],
+    required: true
+  },
+    description: {
+    type: String,
+    default:''
+  },
+    blance: {
+    type: Number,
+    defalt:0
+  },
+  }, {
+  timestamps: true
+ });
+const PointsHistory = mongo.model('PointsHistory', PointsHistorySchema);
+
+module.exports = PointsHistory;
