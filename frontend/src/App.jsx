d7c16a61cfea996317115c7a9a245fecc0cd4231
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-27 21:40:07 +0100

    feat: add sound provider to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import { NotificationsProvider } from './context/NotificationsContext'
+import { SoundProvider } from './context/SoundContext'
 import AppRoutes from './routes'
 import VideoBackground from './components/VideoBackground'
 import MobileNav from './components/MobileNav'
+import SoundController from './components/SoundController'
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <NotificationsProvider>
-        <BrowserRouter>
-          <VideoBackground />
-          <div className="mobile-container">
-            <AppRoutes />
-            <MobileNav />
-          </div>
-        </BrowserRouter>
-      </NotificationsProvider>
-    </ThemeProvider>
+    <SoundProvider>
+      <ThemeProvider>
+        <NotificationsProvider>
+          <BrowserRouter>
+            <VideoBackground />
+            <SoundController />
+            <div className="mobile-container">
+              <AppRoutes />
+              <MobileNav />
+            </div>
+          </BrowserRouter>
+        </NotificationsProvider>
+      </ThemeProvider>
+    </SoundProvider>
   )
 }
