24ee5c45e2d45fe97701525493d19353432932cb
mrabt3475-cpu <mrabt3475@gmail.com>
2026-03-27 15:33:34 +0100

    feat: create auth DTOs

---
+++ backend/src/modules/auth/auth.dto.ts
@@ -0,0 +1,27 @@
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
+
+export class RefreshTokenDto {
+  @IsString()
+  refreshToken: string;
+}
